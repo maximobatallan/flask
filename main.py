1d4a429d6a3a72faf0b4d85de78c302f4c19558a
maximobatallan <71332822+maximobatallan@users.noreply.github.com>
2023-09-25 02:28:27 -0300

    Update main.py

--- main.py
+++ main.py
@@ -22,34 +22,34 @@ def webhook_whatsapp():
             return jsonify({"status": "error", "message": "Verification failed"}), 403
     else:
             
-    def handle_message(request):
-        # Parse Request body in json format
-        body = request.get_json()
-        print(f"request body: {body}")
-    
-        try:
-            # info on WhatsApp text message payload:
-            # https://developers.facebook.com/docs/whatsapp/cloud-api/webhooks/payload-examples#text-messages
-            if body.get("object"):
-                if (
-                    body.get("entry")
-                    and body["entry"][0].get("changes")
-                    and body["entry"][0]["changes"][0].get("value")
-                    and body["entry"][0]["changes"][0]["value"].get("messages")
-                    and body["entry"][0]["changes"][0]["value"]["messages"][0]
-                ):
-                    handle_whatsapp_message(body)
-                return jsonify({"status": "ok"}), 200
-            else:
-                # if the request is not a WhatsApp API event, return an error
-                return (
-                    jsonify({"status": "error", "message": "Not a WhatsApp API event"}),
-                    404,
-                )
-        # catch all other errors and return an internal server error
-        except Exception as e:
-            print(f"unknown error: {e}")
-            return jsonify({"status": "error", "message": str(e)}), 500
+        def handle_message(request):
+            # Parse Request body in json format
+            body = request.get_json()
+            print(f"request body: {body}")
+        
+            try:
+                # info on WhatsApp text message payload:
+                # https://developers.facebook.com/docs/whatsapp/cloud-api/webhooks/payload-examples#text-messages
+                if body.get("object"):
+                    if (
+                        body.get("entry")
+                        and body["entry"][0].get("changes")
+                        and body["entry"][0]["changes"][0].get("value")
+                        and body["entry"][0]["changes"][0]["value"].get("messages")
+                        and body["entry"][0]["changes"][0]["value"]["messages"][0]
+                    ):
+                        handle_whatsapp_message(body)
+                    return jsonify({"status": "ok"}), 200
+                else:
+                    # if the request is not a WhatsApp API event, return an error
+                    return (
+                        jsonify({"status": "error", "message": "Not a WhatsApp API event"}),
+                        404,
+                    )
+            # catch all other errors and return an internal server error
+            except Exception as e:
+                print(f"unknown error: {e}")
+                return jsonify({"status": "error", "message": str(e)}), 500
 #INICIAMSO FLASK
 if __name__ == "__main__":
   app.run(debug=True)

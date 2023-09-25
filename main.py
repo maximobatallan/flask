8dad179e9a2aed2e4c828c4786c12804701db77b
maximobatallan <71332822+maximobatallan@users.noreply.github.com>
2023-09-25 02:41:27 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,36 +2,11 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 #CUANDO RECIBAMOS LAS PETICIONES EN ESTA RUTA
 @app.route("/webhook/", methods=["POST", "GET"])
-def webhook_whatsapp():
-    #SI HAY DATOS RECIBIDOS VIA GET
- 
-    # Parse params from the webhook verification request
-    mode = request.args.get("hub.mode")
-    token = request.args.get("hub.verify_token")
-    challenge = request.args.get("hub.challenge")
-    # Check if a token and mode were sent
-    if mode and token:
-        # Check the mode and token sent are correct
-        if mode == "subscribe" and token == "river":
-            # Respond with 200 OK and challenge token from the request
-            print("WEBHOOK_VERIFIED")
-            return challenge, 200
-        else:
-            # Responds with '403 Forbidden' if verify tokens do not match
-            print("VERIFICATION_FAILED")
-            return jsonify({"status": "error", "message": "Verification failed"}), 403
-    else:
-            
-       
-        # Responds with '400 Bad Request' if verify tokens do not match
-        print("MISSING_PARAMETER")
-        return jsonify({"status": "error", "message": "Missing parameters"}), 400
-
 def handle_message(request):
     # Parse Request body in json format
     body = request.get_json()
     print(f"request body: {body}")
-
+    print("MISSING_PARAMETER")
     try:
         # info on WhatsApp text message payload:
         # https://developers.facebook.com/docs/whatsapp/cloud-api/webhooks/payload-examples#text-messages

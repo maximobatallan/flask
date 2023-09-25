11bccc21e78e86d64395ba345f3dad622eea54f0
maximobatallan <71332822+maximobatallan@users.noreply.github.com>
2023-09-25 02:15:24 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,45 +4,26 @@ app = Flask(__name__)
 @app.route("/webhook/", methods=["POST", "GET"])
 def webhook_whatsapp():
     #SI HAY DATOS RECIBIDOS VIA GET
-    if request.method == "GET":
-        #SI EL TOKEN ES IGUAL AL QUE RECIBIMOS
-        if request.args.get('hub.verify_token') == "retobot":
-            #ESCRIBIMOS EN EL NAVEGADOR EL VALOR DEL RETO RECIBIDO DESDE FACEBOOK
-            return request.args.get('hub.challenge')
+    def verify(request):
+    # Parse params from the webhook verification request
+    mode = request.args.get("hub.mode")
+    token = request.args.get("hub.verify_token")
+    challenge = request.args.get("hub.challenge")
+    # Check if a token and mode were sent
+    if mode and token:
+        # Check the mode and token sent are correct
+        if mode == "subscribe" and token == river:
+            # Respond with 200 OK and challenge token from the request
+            print("WEBHOOK_VERIFIED")
+            return challenge, 200
         else:
-            #SI NO SON IGUALES RETORNAMOS UN MENSAJE DE ERROR
-          return "Error de autentificacion."
-    #RECIBIMOS TODOS LOS DATOS ENVIADO VIA JSON
-    data=request.get_json()
-    #EXTRAEMOS EL NUMERO DE TELEFONO Y EL MANSAJE
-    telefonoCliente=data
-    #EXTRAEMOS EL TELEFONO DEL CLIENTE
-    mensaje=data
-    if mensaje is not None:
-      
-      import mysql.connector
-      mydb = mysql.connector.connect(
-          host = "containers-us-west-111.railway.app",
-          user = "root",
-          password = "gnEfc1BQNlv2sA8RStCJ",
-          database='railway'
-      )
-      mycursor = mydb.cursor()
-      query="SELECT count(id) AS cantidad FROM registro WHERE id_wa='" + idWA + "';"
-      mycursor.execute("SELECT count(id) AS cantidad FROM registro WHERE id_wa='" + idWA + "';")
-
-      cantidad, = mycursor.fetchone()
-      cantidad=str(cantidad)
-      cantidad=int(cantidad)
-      if cantidad==0 :
-        sql = ("INSERT INTO registro"+ 
-        "(mensaje_recibido,mensaje_enviado,id_wa      ,timestamp_wa   ,telefono_wa) VALUES "+
-        "('"+mensaje+"'   ,'"+respuesta+"','"+idWA+"' ,'"+timestamp+"','"+telefonoCliente+"');")
-        mycursor.execute(sql)
-        mydb.commit()
-
-      #RETORNAMOS EL STATUS EN UN JSON
-      return jsonify({"status": "success"}, 200)
+            # Responds with '403 Forbidden' if verify tokens do not match
+            print("VERIFICATION_FAILED")
+            return jsonify({"status": "error", "message": "Verification failed"}), 403
+    else:
+        # Responds with '400 Bad Request' if verify tokens do not match
+        print("MISSING_PARAMETER")
+        return jsonify({"status": "error", "message": "Missing parameters"}), 400
 
 #INICIAMSO FLASK
 if __name__ == "__main__":

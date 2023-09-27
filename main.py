40ea4b7e4830d38fb6fdf0bb3fc4c05553296d45
maximobatallan <71332822+maximobatallan@users.noreply.github.com>
2023-09-27 02:04:31 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,31 @@
 from flask import Flask, jsonify, request
+
 app = Flask(__name__)
 #CUANDO RECIBAMOS LAS PETICIONES EN ESTA RUTA
 @app.route("/webhook/", methods=["POST", "GET"])
-def handle_message(request):
-    # Parse Request body in json format
-    body = request.get_json()
-    print(f"request body: {body}")
-    print("MISSING_PARAMETER")
-    try:
-        # info on WhatsApp text message payload:
-        # https://developers.facebook.com/docs/whatsapp/cloud-api/webhooks/payload-examples#text-messages
-        if body.get("object"):
-            if (
-                body.get("entry")
-                and body["entry"][0].get("changes")
-                and body["entry"][0]["changes"][0].get("value")
-                and body["entry"][0]["changes"][0]["value"].get("messages")
-                and body["entry"][0]["changes"][0]["value"]["messages"][0]
-            ):
-                handle_whatsapp_message(body)
-            return jsonify({"status": "ok"}), 200
+def verify(request):
+    # Parse params from the webhook verification request
+    mode = request.args.get("hub.mode")
+    token = request.args.get("hub.verify_token")
+    challenge = request.args.get("hub.challenge")
+    # Check if a token and mode were sent
+    if mode and token:
+        # Check the mode and token sent are correct
+        if mode == "subscribe" and token == "Retobao":
+            # Respond with 200 OK and challenge token from the request
+            print("WEBHOOK_VERIFIED")
+            return challenge, 200
         else:
-            # if the request is not a WhatsApp API event, return an error
-            return (
-                jsonify({"status": "error", "message": "Not a WhatsApp API event"}),
-                404,
-            )
-    # catch all other errors and return an internal server error
-    except Exception as e:
-        print(f"unknown error: {e}")
-        return jsonify({"status": "error", "message": str(e)}), 500
+            # Responds with '403 Forbidden' if verify tokens do not match
+            print("VERIFICATION_FAILED")
+            return jsonify({"status": "error", "message": "Verification failed"}), 403
+    else:
+        # Responds with '400 Bad Request' if verify tokens do not match
+        print("MISSING_PARAMETER")
+        return jsonify({"status": "error", "message": "Missing parameters"}), 400
+    
+    
+    
 #INICIAMSO FLASK
 if __name__ == "__main__":
   app.run(debug=True)

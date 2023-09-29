6033319365cad80777b1c67b0eac070b77461d2f
maximobatallan <71332822+maximobatallan@users.noreply.github.com>
2023-09-28 23:09:43 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify
+import requests
 import json
 app = Flask(__name__)
 #CUANDO RECIBAMOS LAS PETICIONES EN ESTA RUTA
 def handle_message():
-    # Parse request body in json format
+    # Parse requests body in json format
     url = "https://graph.facebook.com/v17.0/137446296107512/messages"
 
     payload = json.dumps({
@@ -21,22 +22,22 @@ def handle_message():
       'Authorization': 'Bearer EAAVzJc6WKFUBO8FqpQZCLOxMZAY7Qvioxv1jFx2p5jEyMXSKCg3RC2ngZBg9MRbSdFeSfGhpDpMBBfqWTcCECvpzj27exeMashZAD2ZA6b24YBRwW9t3ZCiY0sfHn2pt2FvKHEmpUemhZAB78n8ezTjzkZAkDxZAZBVhdHGRvQfb2NTGxu5Gdfj67VjZBkOZCNwuqFG0IIzDFiSJUg71jGS48mqm12NhpEsZD'
     }
     
-    response = request.requests("POST", url, headers=headers, data=payload)
+    response = requests.request("POST", url, headers=headers, data=payload)
     print('esta aca')
     return jsonify({"status": "success"}), 200
 
 
-def verify(request):
-    # Parse params from the webhook verification request
-    mode = request.args.get("hub.mode")
-    token = request.args.get("hub.verify_token")
-    challenge = request.args.get("hub.challenge")
+def verify(requests):
+    # Parse params from the webhook verification requests
+    mode = requests.args.get("hub.mode")
+    token = requests.args.get("hub.verify_token")
+    challenge = requests.args.get("hub.challenge")
     # Check if a token and mode were sent
-    if request.method == "GET":
+    if requests.method == "GET":
         if mode and token:
             # Check the mode and token sent are correct
             if mode == "subscribe" and token == "Retobao":
-                # Respond with 200 OK and challenge token from the request
+                # Respond with 200 OK and challenge token from the requests
                 print("WEBHOOK_VERIFIED")
                 return challenge, 200
             else:
@@ -44,10 +45,10 @@ def verify(request):
                 print("VERIFICATION_FAILED")
                 return jsonify({"status": "error", "message": "Verification failed"}), 403
         else:
-            # Responds with '400 Bad request' if verify tokens do not match
+            # Responds with '400 Bad requests' if verify tokens do not match
             print("MISSING_PARAMETER")
             return jsonify({"status": "error", "message": "Missing parameters"}), 400
-    data=request.get_json()
+    data=requests.get_json()
     #EXTRAEMOS EL NUMERO DE TELEFONO Y EL MANSAJE
     mensaje="Telefono:"+data['entry'][0]['changes'][0]['value']['messages'][0]['from']
     mensaje=mensaje+"|Mensaje:"+data['entry'][0]['changes'][0]['value']['messages'][0]['text']['body']
@@ -59,10 +60,10 @@ def verify(request):
 
 @app.route("/webhook", methods=["POST", "GET"])
 def webhook():
-    if request.method == "GET":    
+    if requests.method == "GET":    
         print('get')
-        return verify(request)
-    elif request.method == "POST":
+        return verify(requests)
+    elif requests.method == "POST":
         print('post')
         return handle_message()
 

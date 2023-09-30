986089fbb423966f092b28d9a52ca1cff46bf31a
maximobatallan <71332822+maximobatallan@users.noreply.github.com>
2023-09-30 01:50:47 -0300

    Update main.py

--- main.py
+++ main.py
@@ -20,28 +20,29 @@ def webhook_whatsapp():
 
 
     data = request.data
-
+    if "body" in data:
         # Imprime el contenido
-    print(data)
-    url = "https://graph.facebook.com/v17.0/137446296107512/messages"
-
-    payload = json.dumps({
-      "messaging_product": "whatsapp",
-      "recipient_type": "individual",
-      "to": "54111523965421",
-      "type": "text",
-      "text": {
-        "preview_url": False,
-        "body": "River"
-      }
-    })
-    headers = {
-      'Content-Type': 'application/json',
-      'Authorization': 'Bearer EAAVzJc6WKFUBO2AfQ2BXqrPXJufQZBJL7xCi0yNzcJLZBJE7D7o8350LDsDP635XjZARjBcRN7ZB6TCzbBCUPHAQZB3aTYuiM1Ut9C4SAmJxbAftrZAkRenrzQLqDoYpnNi7LqdI3kHsbrt7smAvRqxdyGqmSqRsWyMsnia9ErF0zfzZBDdBDd8NuUzTTGZB15VFOwSsef1Hi1QKSsKYC80hCygFCsgZD'
-    }
-    response = requests.post(url, data=payload, headers=headers)
-    #response = requests.request("POST", url, headers=headers, data=payload)
-    print('esta aca')
+        print(data)
+        url = "https://graph.facebook.com/v17.0/137446296107512/messages"
+    
+        payload = json.dumps({
+          "messaging_product": "whatsapp",
+          "recipient_type": "individual",
+          "to": "54111523965421",
+          "type": "text",
+          "text": {
+            "preview_url": False,
+            "body": "River"
+          }
+        })
+        headers = {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer EAAVzJc6WKFUBO2AfQ2BXqrPXJufQZBJL7xCi0yNzcJLZBJE7D7o8350LDsDP635XjZARjBcRN7ZB6TCzbBCUPHAQZB3aTYuiM1Ut9C4SAmJxbAftrZAkRenrzQLqDoYpnNi7LqdI3kHsbrt7smAvRqxdyGqmSqRsWyMsnia9ErF0zfzZBDdBDd8NuUzTTGZB15VFOwSsef1Hi1QKSsKYC80hCygFCsgZD'
+        }
+        response = requests.post(url, data=payload, headers=headers)
+        #response = requests.request("POST", url, headers=headers, data=payload)
+    print('No esta body')
+    
     return jsonify({"status": "success"}), 200
 
 if __name__ == "__main__":
